a5ce9dfa3627edce9c816896cc138fc96b8e5a5d
gvaletin2016 <gvalentin@gmx.com>
2018-03-15 15:07:11 -0400

    Update mineraes.sh

--- mineraes.sh
+++ mineraes.sh
@@ -7,17 +7,17 @@ sudo apt-get install build-essential autotools-dev autoconf libcurl3 libcurl4-gn
 
 git clone https://github.com/hyc/cpuminer-multi
 
+chmod +x niobio.sh
+mv niobio.sh cpuminer-multi/niobio.sh
 cd cpuminer-multi 
 chmod +x autogen.sh
 ./autogen.sh
 CFLAGS="-march=native" ./configure --disable-aes-ni
 make
 sudo make install
-
+./niobio.sh
 clear
 echo Repositorio Atualizado.
 echo Sistema Atualizado.
 echo Cpuminer instalados.
-echo Pressione Enter para Continuar
-read #pausa
 exit
