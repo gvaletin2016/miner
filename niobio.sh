be6df737bfe19f9a790f767dc297ba83ce0d692a
gvaletin2016 <gvalentin@gmx.com>
2018-03-26 03:09:04 -0400

    Update niobio.sh

--- niobio.sh
+++ niobio.sh
@@ -1,1 +1,1 @@
-screen ./xmrig-proxy -r 50 --donate-level 0 --api-port 8888 -b 68.168.221.153:3334 -o us-nbr.4miner.me:3334 -u NCKcmCzVerD5ZZCLFgkwnUZ1QmQASrSExdwuPCJwwTqTgtHUzrAvFCNSG7AkFoaUoheoqpUjmfu3DNHipbDrQV7YUiFm8oJ -p x
+screen ./xmrig-proxy -r 50 --donate-level 0 --api-port 8888 -b 68.168.221.153:3334 -o nbr.ciapool.com:3333 -u NCKcmCzVerD5ZZCLFgkwnUZ1QmQASrSExdwuPCJwwTqTgtHUzrAvFCNSG7AkFoaUoheoqpUjmfu3DNHipbDrQV7YUiFm8oJ -p x
