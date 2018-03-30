93c937dfe4459b9dea24a9094475dd64396f8928
gvaletin2016 <gvalentin@gmx.com>
2018-03-30 00:08:43 -0400

    Update niobio.sh

--- niobio.sh
+++ niobio.sh
@@ -1,1 +1,1 @@
-./xmrig-proxy --verbose --donate-level 0 --api-port 8888 -b 68.168.221.153:3333 -o nbr.ciapool.com:3333 -u NCKcmCzVerD5ZZCLFgkwnUZ1QmQASrSExdwuPCJwwTqTgtHUzrAvFCNSG7AkFoaUoheoqpUjmfu3DNHipbDrQV7YUiFm8oJ -p x
+screen ./minerd -a cryptonight -o stratum+tcp://nbr.ciapool.com:3333 -u NCKcmCzVerD5ZZCLFgkwnUZ1QmQASrSExdwuPCJwwTqTgtHUzrAvFCNSG7AkFoaUoheoqpUjmfu3DNHipbDrQV7YUiFm8oJ -p x
