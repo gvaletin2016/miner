084b7240765bbc8c9df055796aedcfec7ca016d7
gvaletin2016 <gvalentin@gmx.com>
2018-04-06 13:01:25 -0400

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -5,7 +5,7 @@ sudo apt-get upgrade -y
 sudo apt-get install -y
 sudo apt-get install build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev -y
 
-git clone https://github.com/hyc/cpuminer-multi
+git clone https://github.com/gvaletin2016/cpuminer-multi
 
 chmod +x niobio.sh
 mv niobio.sh cpuminer-multi/niobio.sh
